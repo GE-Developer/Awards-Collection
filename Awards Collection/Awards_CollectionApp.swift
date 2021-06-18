37c7af2af4fc093303b54f425652e44c575794c5
Michael Bukhrashvili <on-line-misha@mail.ru>
2021-06-18 19:14:10 +0300

    Initial Commit

---
+++ Awards Collection/Awards_CollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Awards_CollectionApp.swift
+//  Awards Collection
+//
+//  Created by Mikhail Bukhrashvili on 18.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Awards_CollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
